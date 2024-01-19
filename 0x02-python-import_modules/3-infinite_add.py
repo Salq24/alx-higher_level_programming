3ceb52eaa03fb60134c26a470ae84cbb0a70f074
Salq24 <qogheneochuko@gmail.com>
2024-01-19 07:35:28 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 def arg_add(args):
-    result = sum(int(a) for a in args )
+    result = sum(int(a) for a in args)
     print(result)
 if __name__ == "__main__":
     arg_add(sys.argv[1:])
