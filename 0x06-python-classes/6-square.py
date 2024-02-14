5b315bc2adacdda455be5bb2c69f99d55a493f4f
Salq24 <qogheneochuko@gmail.com>
2024-02-14 11:36:52 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Create a square class"""
 class Square:
+    """This is the class"""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
