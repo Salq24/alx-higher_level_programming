a17e3ae0b49b7d295486b22253cd4bfc4fe6cb3b
Salq24 <qogheneochuko@gmail.com>
2024-02-15 10:34:48 -0800

    Test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,8 @@
 """ This module has a function named say_my_name. It prints
 My name is <first name> <last name>
 """
+
+
 def say_my_name(first_name, last_name=""):
     """
     Prints 'My name is <first name> <last name>'
