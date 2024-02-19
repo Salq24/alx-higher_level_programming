b8afccc22b5d31ff8213406779fc505af68ece70
Salq24 <qogheneochuko@gmail.com>
2024-02-19 10:43:25 -0800

    more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     """This is the class built.
     Attributes: width and height
     Raises:
-    ValueError , if width or height are 
+    ValueError , if width or height are
     less than zero.
     TypeError, if width and height are not integers
     """
@@ -20,6 +20,7 @@ class Rectangle:
         @property
         def width(self):
             return self.__width
+
         @width.setter
         def width(self, value):
             if value < 0:
@@ -31,6 +32,7 @@ class Rectangle:
         @property
         def height(self):
             return self.__height
+
         @height.setter
         def height(self, value):
             if not isinstance(value, int):
@@ -38,4 +40,3 @@ class Rectangle:
             if value < 0:
                 raise ValueError("height must be >= 0")
             self.__height = value
-
