f9fbaa908403a24d420601180cad191e8e6d8ec3
Salq24 <qogheneochuko@gmail.com>
2024-02-24 02:59:08 -0800

    input

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Thismodule has a function pascal_triangle
+that returns a list of lists of integers
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    pas = []
+    for x in range(n):
+        row = [1]
+        if x > 0:
+            row_p = pas[-1]
+            for w in range(1, x):
+                row.append(row_p[w - 1] + row_p[w])
+            row.append(1)
+        pas.append(row)
+
+    return pas
