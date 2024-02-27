7d055bbda9f8c8a8296f6855e658a04665e0a6ac
queen <qogheneochuko@gmail.com>
2024-02-27 11:10:01 -0800

    rect

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,3 +46,15 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == 'Rectangle':
+            second = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            second = cls(1, 1)
+        else:
+            return None
+        second.update(**dictionary)
+        return second
