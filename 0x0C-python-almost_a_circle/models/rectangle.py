28d0c58548ec5df381cdde616b7c86af5eb4b3b5
queen <qogheneochuko@gmail.com>
2024-02-27 09:17:52 -0800

    rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,6 +90,19 @@ class Rectangle(Base):
         for x in range(self.height):
             print(" " * self.x + "#" * self.width)
 
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+
     def __str__(self):
         """returns the str rep of the rect instance"""
         return "[Rectangle] ({}) {}/{} - {}/{}".\
