1fa65fb476d5cc921518f7a08cd91a13266cfb31
queen <qogheneochuko@gmail.com>
2024-02-27 06:59:16 -0800

    circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This module has a class, Base
+"""
 class Base:
+    """This is the class,
+    with private class attribute and
+    class constructor
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
