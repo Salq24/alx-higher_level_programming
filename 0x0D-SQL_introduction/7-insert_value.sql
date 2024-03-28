3d81e6fbe4337664b0ba92060f3045b68afda7f7
Salq24 <qogheneochuko@gmail.com>
2024-03-28 09:40:52 -0700

    databases

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Inserts a new row in the tqable
+
+INSERT INTO first_table(
+	id, name)
+VALUES(89, 'Best School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Displays the num of records with id = 89
+
+SELECT COUNT(*) FROM first_table
+WHERE id = 89;
