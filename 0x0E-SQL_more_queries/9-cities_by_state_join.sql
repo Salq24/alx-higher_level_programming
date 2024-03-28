3ac2453e3d60fc33ab448ebe085fa0e2b026b0dd
Salq24 <qogheneochuko@gmail.com>
2024-03-28 12:48:38 -0700

    0-priveledges

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- list all cities
 
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
+
