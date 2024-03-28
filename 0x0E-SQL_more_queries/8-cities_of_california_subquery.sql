1d0d8ca10271e533496de0e417fdc9ed3562e4e9
Salq24 <qogheneochuko@gmail.com>
2024-03-28 12:44:08 -0700

    0-priveledges

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- lists cities in carlifornia without using joins
+
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+AND states.name = 'California'
+ORDER BY cities.id ASC;
+
