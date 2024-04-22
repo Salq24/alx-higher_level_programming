e3d5cd84efc8f09f34792f8a7e6c87a77b1689c3
Queen <qogheneochuko@gmail.com>
2024-04-22 10:25:30 -0700

    multi

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arrLoop =  ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrLoop = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let x = 0; x < arrLoop.length; x++) {
   console.log(arrLoop[x]);
