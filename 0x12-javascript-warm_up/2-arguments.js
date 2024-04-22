714913ef3fccf9a9fd6b8c729ea6e83d41755f6f
Queen <qogheneochuko@gmail.com>
2024-04-22 09:26:07 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,10 @@
 
 const cnt = process.argv.length;
 
-if (cnt === 2){
-	console.log('No argument');
-} else if (cnt === 3){
-	console.log('Argument found');
+if (cnt === 2) {
+  console.log('No argument');
+} else if (cnt === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');}
+  console.log('Arguments found');
+}
