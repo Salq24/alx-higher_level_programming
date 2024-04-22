61201d64c06ef51176163b99ee7b2c6fbf3646fd
Queen <qogheneochuko@gmail.com>
2024-04-22 10:04:02 -0700

    Arguments

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const conv = process.argv[2];
 const num = parseInt(conv);
 
 if (!isNaN(num)) {
-  console.log('My number:',num);
+  console.log('My number:', num);
 } else {
   console.log('Not a number');
 }
