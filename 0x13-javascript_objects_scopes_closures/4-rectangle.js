c5ae26e678d81eb09b570a138c4e498aff34006c
Queen <qogheneochuko@gmail.com>
2024-04-23 07:07:12 -0700

    Rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,11 +15,11 @@ class Rectangle {
       console.log(rec);
     }
   }
-  
+
   rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
-  
+
   double () {
     this.width *= 2;
     this.height *= 2;
