c51b0dc17f48c6fd013def186ce0de7a177a0af5
Queen <qogheneochuko@gmail.com>
2024-04-23 07:05:35 -0700

    Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return ('Rectangle {}');
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    const rec = 'X'.repeat(this.width);
+    for (let y = 0; y < this.height; y++) {
+      console.log(rec);
+    }
+  }
+  
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+  
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
