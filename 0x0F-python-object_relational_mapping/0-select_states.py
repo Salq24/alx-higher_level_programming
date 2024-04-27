009eedae83077714eef81e58b923e5ea2135d008
Salq24 <qogheneochuko@gmail.com>
2024-04-27 07:10:49 -0700

    mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    con_db = MySQLdb.connect(
+            host="localhost", user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = con_db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for row in states:
+        print(row)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+Python object relationa;l mapping
