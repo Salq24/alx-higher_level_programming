de29c918a8c14ae8cdb15e643fbf0dcd938d173e
Salq24 <qogheneochuko@gmail.com>
2024-04-27 09:09:26 -0700

    mapping

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    """Access the database"""
+    link_db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(link_db)
+
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    state_n = session.query(State).filter(State.id == 2).first()
+    state_n.name = "New Mexico"
+    session.commit()
+
+    session.close()
