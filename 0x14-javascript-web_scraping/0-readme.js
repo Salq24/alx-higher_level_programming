55b02ebae0b7ae6682c62980497f00a1c00a983f
Salq24 <qogheneochuko@gmail.com>
2024-06-04 11:24:39 -0700

    cisfun

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
-//reads and prints file contene
-
+// reads and prints file contene
 
 const fsys = require('fs');
 const filenm = process.argv[2];
 
 fsys.readFile(filenm, 'utf-8', (error, data) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(data);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
-
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-
 const fsys = require('fs');
 const filenm = process.argv[2];
 const data = process.argv[3];
 
 fsys.writeFile(filenm, data, 'utf-8', (error) => {
-	if (error) {
-		console.log(error);
-	}
+  if (error) {
+    console.log(error);
+  }
 });
-
