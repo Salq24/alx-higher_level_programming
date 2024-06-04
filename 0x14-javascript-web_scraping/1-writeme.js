a30fea2842aab43c57d3855b50fe364a644c8f39
Salq24 <qogheneochuko@gmail.com>
2024-06-04 11:15:26 -0700

    cisfun

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//reads and prints file contene
+
+
+const fsys = require('fs');
+const filenm = process.argv[2];
+const data = process.argv[3];
+
+fsys.writeFile(filenm, data, 'utf-8', (error) => {
+	if (error) {
+		console.log(error);
+	}
+});
+
