134cb77e55e62b6e155838cd38ae32a4abc19e7c
Salq24 <qogheneochuko@gmail.com>
2024-06-06 01:56:27 -0700

    add

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,22 +3,30 @@
 const request = require('request');
 const movieid = process.argv[2];
 const link = `https://swapi.dev/api/films/${movieid}/`;
+let xters = [];
 
-request.get(link, (err, response, data) => {
+request(link, (err, response, data) => {
   if (err) {
     console.log(err);
-  } else {
-    const movie = JSON.parse(data);
-    const xters = movie.characters;
-    for (const xter of xters) {
-      request.get(xter, (err, response, data) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const xterdata = JSON.parse(data);
-          console.log(xterdata.name);
-        }
-      });
-    }
+    return;
   }
+
+  const movie = JSON.parse(data);
+  xters = movie.characters;
+  xterget(0);
 });
+
+const xterget = (ind) => {
+  if (ind === xters.length) {
+    return;
+  }
+  request.get(xters, (err, response, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const xterdata = JSON.parse(data);
+    console.log(xterdata.name);
+    xterget(ind + 1);
+  });
+};
